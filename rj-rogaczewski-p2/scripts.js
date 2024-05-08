b1330a38e6d5b79d9ad2e17094eb0b7dc8160fdf
rjrogaczewski <158858920+rjrogaczewski@users.noreply.github.com>
2024-05-07 19:15:36 -0500

    scripts.js

--- rj-rogaczewski-p2/scripts.js
+++ rj-rogaczewski-p2/scripts.js
@@ -1,33 +1,69 @@
-Correct1.onclick = function() {
-    Correct1.style.backgroundColor = 'green';
-    decision1.innerHTML = "Correct Answer"
-};
-for (let i = 0; i < Incorrect1.length; i++) {
-    Incorrect1[i].onclick = function() {
-        Incorrect1[i].style.backgroundColor = 'red';
-        decision1.innerHTML = "Incorrect Answer"
-    };
+// Initializing variables
+let score = 0;
+let currentQ = 0;
+let questions = ["What is the name of the Major League Baseball team located in Los Angeles?",
+"What NBA team does Stephen Curry play for?",
+"When did the Milwaukee Bucks last win the NBA Finals?"];
+let length = questions.length;
+
+// Styling the Game by calling the classes
+function sportsTrivia() {
+    let getQ = document.getElementById("questions")
+    getQ.innerHTML = questions[0];
+    document.getElementById("answerQ").style = "block";
+    document.getElementById("userInput").style = "block";
+    document.getElementById("gameScore").style = "block";
+    document.getElementById("confirmAnswer").style = "block";
 }
 
-Correct2.onclick = function() {
-    Correct2.style.backgroundColor = 'green';
-    decision2.innerHTML = "Correct Answer"
-};
-for (let j = 0; j < Incorrect2.length; j++) {
-    Incorrect2[j].onclick = function() {
-        Incorrect2[j].style.backgroundColor = 'red';
-        decision2.innerHTML = "Incorrect Answer"
-    };
-}
+// Start of the game
+function gameStart() {
+    let gameQ = document.getElementById("questions");
+    let userAns = document.getElementById("userInput");
 
-Correct3.onclick = function() {
-    Correct3.style.backgroundColor = 'green';
-    decision3.innerHTML = "Correct Answer"
-};
-for (let f = 0; f < Incorrect3.length; f++) {
-    Incorrect3[f].onclick = function() {
-        Incorrect3[f].style.backgroundColor = 'red';
-        decision3.innerHTML = "Incorrect Answer"
-    };
+    // Using If and Else If statements to determine what question and if they got it right
+    if (gameQ.innerHTML === "What is the name of the Major League Baseball team located in Los Angeles?" && userAns.value  === "Dodgers") {
+        alert("You got it Correct!!!");
+        currentQ++;
+        gameQ.innerHTML = questions[currentQ];
+        userAns.value = null;
+        score++;
+        document.getElementById("gameScore").innerHTML = "Game Score: " + score;
+    }
+    else if (gameQ.innerHTML === "What NBA team does Stephen Curry play for?" && userAns.value === "Warriors") {
+        alert("Yessir!!");
+        currentQ++;
+        gameQ.innerHTML = questions[currentQ];
+        userAns.value = null;
+        score++;
+        document.getElementById("gameScore").innerHTML = "Game Score: " + score;
+    }
+    else if (gameQ.innerHTML === "When did the Milwaukee Bucks last win the NBA Finals?" && userAns.value === "2021") {
+        alert("Great Job!");
+        userAns.value = null;
+        document.getElementById("answerQ").style.display = "none"
+        document.getElementById("userInput").style.display = "none"
+        document.getElementById("confirmAnswer").style.display = "none"
+        score++;
+        gameQ.innerHTML = "Game Over. How'd you do???";
+        document.getElementById("gameScore").innerHTML = "Game Score: " + score;
+    }
+    // Else statement used for when the user is wrong
+    else {
+        alert("Oh no, you don't know your sports... Darn!");
+        userAns.value = null;
+        document.getElementById("gameScore").innerHTML = "Game Score: " + score;
+        if (currentQ < length-1) {
+            currentQ++;
+            gameQ.innerHTML = questions[currentQ];
+        }
+        else {
+        document.getElementById("answerQ").style.display = "none"
+        document.getElementById("userInput").style.display = "none"
+        document.getElementById("confirmAnswer").style.display = "none"
+        gameQ.innerHTML = "Game Over. How'd you do???";
+        document.getElementById("startNewGame").innerHTML = "If you would like to play again, refresh this page."
+        document.getElementById("gameScore").innerHTML = "Game Score: " + score;
+        }
+    }
 }
-
